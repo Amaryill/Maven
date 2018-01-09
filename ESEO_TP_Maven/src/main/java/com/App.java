49dc60bfb4a3ef070e19e7508f39b56bcb20b422
Amaryill-Labtop <Amaryill-Labtop@DESKTOP-IVH3365>
2018-01-09 12:04:42 +0100

    test commit 3

--- ESEO_TP_Maven/src/main/java/com/App.java
+++ ESEO_TP_Maven/src/main/java/com/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
     	// affichge dans la console
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! test" );
         
         Operations operations = new Operations();
         long total = operations.additionner(3, 2);
